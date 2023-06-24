a4af04087e95ece00cb6d2b8830d9a1cb8b7e519
Hendrick <hendrickmalatji017@gmail.com>
2023-06-24 06:05:42 +0200

    submit.php

---
+++ submit.php
@@ -0,0 +1,2 @@
+$sql = "INSERT INTO form_data (name, surname, organization, mobile_number, alternative_number, email, address, region, commission, date)
+        VALUES ('$name', '$surname', '$organization', '$mobile_number', '$alternative_number', '$email', '$address', '$region', '$commission', '$date')";
